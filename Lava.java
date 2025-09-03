2aa0491615812c0bac5a0f5d3886975f0bc848d6
erlmek <erlm@ek.dk>
2025-09-03 23:43:25 +0200

    en java source fil

---
+++ Lava.java
@@ -0,0 +1,18 @@
+//package JavaX.Source
+
+import java.math.BigInteger;
+
+  public class Lava {
+
+    public static void main(String[] args) {
+ 	  System.out.println("Helle world I am Lava!");
+	  BigInteger big = BigInteger.valueOf(1000);
+	  System.out.println("jeg er big" + big);
+
+    }
+
+    public Lava() {
+      super();
+    }
+  }
+
